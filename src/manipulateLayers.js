579ef3c7ac235a87bda154c31af202b3557afb12
Henrik Uggla <henrik.uggla@kristianstad.se>
2025-11-25 11:00:26 +0100

    Update manipulateLayers.js

--- src/manipulateLayers.js
+++ src/manipulateLayers.js
@@ -21,6 +21,7 @@ const ManipulateLayers = function ManipulateLayers(viewer, origoConfig) {
     });
 
     _viewer.addLayers(swiperLayers);
+    setTimeout(() => {}, 0);
 
     return swiperLayers;
   }
